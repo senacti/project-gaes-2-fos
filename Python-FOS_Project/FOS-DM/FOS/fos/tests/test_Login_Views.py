226b4f7f7d24589ac28006911ca1d5a7fce56ec1
DanielMarinG <mlbbdaniel30@gmail.com>
2024-02-28 16:31:01 -0500

    pytest login prueba

--- Python-FOS_Project/FOS-DM/FOS/fos/tests/test_Login_Views.py
+++ Python-FOS_Project/FOS-DM/FOS/fos/tests/test_Login_Views.py
@@ -1,4 +1,37 @@
 import pytest
 
-from fos.fos.views import login_view
+
+
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class LoginViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('login')
+        self.user_data = {
+            'username': 'testuser',
+            'password': 'testpassword',
+        }
+        self.user = User.objects.create_user(**self.user_data)
+
+    def test_login_successful(self):
+        response = self.client.post(self.login_url, self.user_data, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated)
+        self.assertRedirects(response, reverse('ventas'))
+        self.assertContains(response, 'Bienvenido')
+
+    def test_login_unsuccessful(self):
+        # Intenta iniciar sesión con credenciales incorrectas
+        response = self.client.post(self.login_url, {'username': 'incorrect', 'password': 'incorrect'})
+        self.assertFalse(response.context['user'].is_authenticated)
+        self.assertContains(response, 'Usuario o contraseña incorrectos')
+
+    def test_login_view_rendered_correctly(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)  # 200: OK
+        self.assertTemplateUsed(response, 'login.html')
+
+
 
